26d02c8e5041902e7cfa46dd0157042a96b65049
dudcheol <parkyc0301@gmail.com>
2020-02-14 23:33:34 +0900

    메모편집 - 누락된부분 커밋(삭제버튼)

--- LineMemo/app/src/main/java/com/example/linememo/ImageAdapter.java
+++ LineMemo/app/src/main/java/com/example/linememo/ImageAdapter.java
@@ -1,25 +1,15 @@
 package com.example.linememo;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -28,10 +18,6 @@ import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 
@@ -53,7 +39,7 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ItemViewHold
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.memo_image_item, parent, false);
-        return  new ItemViewHolder(v);
+        return new ItemViewHolder(v);
     }
 
     @Override
@@ -76,21 +62,12 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ItemViewHold
                 })
                 .into(holder.imageView);
         holder.imageView.setOnClickListener(null); // Todo : 크게보기
-
-//        // 어댑터 모드 분기
-//        if (currentMode == IMAGE_ADAPTER_EDIT_MODE) {
-//            // IMAGE_ADAPTER_EDIT_MODE - 삭제버튼 있어야 함
-//            holder.itemDeleteBtn.setVisibility(View.VISIBLE);
-//            holder.itemDeleteBtn.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    removeImage(position);
-//                }
-//            });
-//        } else {
-//            // IMAGE_ADAPTER_VIEW_MODE - 삭제버튼 없음
-//            holder.itemDeleteBtn.setVisibility(View.GONE);
-//        }
+        holder.itemDeleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                removeImage(position);
+            }
+        });
     }
 
     @Override
