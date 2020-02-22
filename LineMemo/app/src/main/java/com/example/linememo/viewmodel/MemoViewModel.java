1224a6c68e901d1a62c5a594f9a8694b0c6a8624
dudcheol <parkyc0301@gmail.com>
2020-02-23 05:09:27 +0900

    메모뷰모델 - 클릭 콜백 추가, 메모상세를 위한 메소드 추가

--- LineMemo/app/src/main/java/com/example/linememo/viewmodel/MemoViewModel.java
+++ LineMemo/app/src/main/java/com/example/linememo/viewmodel/MemoViewModel.java
@@ -1,17 +1,24 @@
 package com.example.linememo.viewmodel;
 
 import android.app.Application;
+import android.util.Log;
+import android.view.View;
 
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
 import com.example.linememo.model.Memo;
 import com.example.linememo.repository.MemoRepository;
+import com.example.linememo.util.ConvertUtil;
 
 import java.util.List;
 
 public class MemoViewModel extends AndroidViewModel {
+    private static final String TAG = "MemoViewModel";
     private MemoRepository mRepository;
+    private LiveData<Memo> findLiveMemo;
+    private LiveData<Integer> memoCnt;
+    private ButtonClickCallback buttonClickCallback;
 
     public MemoViewModel(Application application) {
         super(application);
@@ -22,8 +29,14 @@ public class MemoViewModel extends AndroidViewModel {
         return mRepository.getAll();
     }
 
+    public LiveData<Integer> getCount() {
+        setMemoCnt(mRepository.getCount());
+        return memoCnt;
+    }
+
     public LiveData<Memo> findLive(int id) {
-        return mRepository.findLive(id);
+        setFindLiveMemo(mRepository.findLive(id));
+        return findLiveMemo;
     }
 
     public Memo find(int id) {
@@ -48,5 +61,50 @@ public class MemoViewModel extends AndroidViewModel {
 
     public int getSavedRecyclerLayoutState() {
         return mRepository.getSavedRecyclerLayoutState();
+    }
+
+    public String convertDate(long date) {
+        if (date == 0) return "";
+        return ConvertUtil.longDateToLongString(date);
+    }
+
+    public int hasImages(List<String> list) {
+        Log.e(TAG, list == null ? "list null" : list.toString());
+        if (list == null || list.isEmpty()) return View.GONE;
+        else return View.VISIBLE;
+    }
+
+    // listener
+    public interface ButtonClickCallback {
+        void clicked();
+    }
+
+    public void doOnClick(ButtonClickCallback buttonClickCallback) {
+        buttonClickCallback.clicked();
+    }
+
+    public void setButtonClickCallback(ButtonClickCallback buttonClickCallback) {
+        this.buttonClickCallback = buttonClickCallback;
+    }
+
+    public ButtonClickCallback getButtonClickCallback() {
+        return this.buttonClickCallback;
+    }
+
+    // getter & setter
+    public LiveData<Memo> getFindLiveMemo() {
+        return findLiveMemo;
+    }
+
+    public LiveData<Integer> getMemoCnt() {
+        return memoCnt;
+    }
+
+    public void setFindLiveMemo(LiveData<Memo> findLiveMemo) {
+        this.findLiveMemo = findLiveMemo;
+    }
+
+    public void setMemoCnt(LiveData<Integer> memoCnt) {
+        this.memoCnt = memoCnt;
     }
 }
