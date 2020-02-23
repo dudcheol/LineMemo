2aaf0496522ca6b58af5bba07903f7ad0dea346f
dudcheol <parkyc0301@gmail.com>
2020-02-23 18:31:05 +0900

    리팩토링 - 포토뷰 데이터바인딩

--- LineMemo/app/src/main/java/com/example/linememo/view/activity/PhotoViewActivity.java
+++ LineMemo/app/src/main/java/com/example/linememo/view/activity/PhotoViewActivity.java
@@ -2,94 +2,76 @@ package com.example.linememo.view.activity;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.example.linememo.R;
+import com.example.linememo.databinding.ActivityPhotoViewBinding;
 import com.example.linememo.util.GlideUtil;
 import com.example.linememo.view.animation.ActivityTransitionAnim;
 import com.github.chrisbanes.photoview.OnScaleChangedListener;
 import com.github.chrisbanes.photoview.OnViewTapListener;
-import com.github.chrisbanes.photoview.PhotoView;
 
 public class PhotoViewActivity extends AppCompatActivity {
     private static final String TAG = "PhotoViewActivity";
-    private PhotoView photoView;
+    private ActivityPhotoViewBinding mBinding;
     private String uri;
-    private ProgressBar progressBar;
-    private ImageView photoViewCloseButton;
-    private TextView photoViewText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo_view);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_photo_view);
 
         initSetting();
         showPhoto();
     }
 
     private void initSetting() {
-        findViewByIds();
-        initData();
+        uri = getIntent().getStringExtra("uri");
         setPhotoView();
         setListener();
     }
 
-    private void findViewByIds() {
-        photoView = findViewById(R.id.photo_view);
-        progressBar = findViewById(R.id.progress_bar);
-        photoViewCloseButton = findViewById(R.id.photo_view_close_button);
-        photoViewText = findViewById(R.id.photo_view_text);
-    }
-
-    private void initData() {
-        uri = getIntent().getStringExtra("uri");
-    }
-
     private void setPhotoView() {
-        photoView.setZoomTransitionDuration(400);
-        photoView.setMaximumScale(5f);
-        photoView.setMediumScale(2f);
-        photoView.setMinimumScale(1f);
+        mBinding.photoView.setZoomTransitionDuration(400);
+        mBinding.photoView.setMaximumScale(5f);
+        mBinding.photoView.setMediumScale(2f);
+        mBinding.photoView.setMinimumScale(1f);
     }
 
     private void setListener() {
-        photoViewCloseButton.setOnClickListener(new View.OnClickListener() {
+        mBinding.photoViewCloseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
 
-        photoView.setOnScaleChangeListener(new OnScaleChangedListener() {
+        mBinding.photoView.setOnScaleChangeListener(new OnScaleChangedListener() {
             @Override
             public void onScaleChange(float scaleFactor, float focusX, float focusY) {
 //                Log.e(TAG, "getcale=" + photoView.getScale());
-                if (photoView.getScale() >= 1.1) {
-                    photoViewCloseButton.setVisibility(View.GONE);
-                    photoViewText.setVisibility(View.GONE);
+                if (mBinding.photoView.getScale() >= 1.1) {
+                    mBinding.photoViewCloseButton.setVisibility(View.GONE);
+                    mBinding.photoViewText.setVisibility(View.GONE);
                 } else {
-                    photoViewCloseButton.setVisibility(View.VISIBLE);
-                    photoViewText.setVisibility(View.VISIBLE);
+                    mBinding.photoViewCloseButton.setVisibility(View.VISIBLE);
+                    mBinding.photoViewText.setVisibility(View.VISIBLE);
                 }
             }
         });
 
-        photoView.setOnViewTapListener(new OnViewTapListener() {
+        mBinding.photoView.setOnViewTapListener(new OnViewTapListener() {
             @Override
             public void onViewTap(View view, float x, float y) {
-                photoView.setScale(1f, true);
+                mBinding.photoView.setScale(1f, true);
             }
         });
     }
@@ -98,7 +80,7 @@ public class PhotoViewActivity extends AppCompatActivity {
         GlideUtil.showAddReqListener(this
                 , uri
                 , new int[]{2048, 2048}
-                , photoView
+                , mBinding.photoView
                 , requestListener
                 , true);
     }
@@ -106,15 +88,15 @@ public class PhotoViewActivity extends AppCompatActivity {
     private RequestListener requestListener = new RequestListener<Drawable>() {
         @Override
         public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-            progressBar.setVisibility(View.GONE);
-            photoViewText.setText(R.string.photo_view_load_fail_notice);
-            photoViewText.setTextColor(getResources().getColor(R.color.colorErr));
+            mBinding.progressBar.setVisibility(View.GONE);
+            mBinding.photoViewText.setText(R.string.photo_view_load_fail_notice);
+            mBinding.photoViewText.setTextColor(getResources().getColor(R.color.colorErr));
             return false;
         }
 
         @Override
         public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-            progressBar.setVisibility(View.GONE);
+            mBinding.progressBar.setVisibility(View.GONE);
             return false;
         }
     };
