77c2980acb7c1d4a47a71adf60e8cf4cb7661954
dudcheol <parkyc0301@gmail.com>
2020-02-23 20:47:56 +0900

    DB명 변경

--- LineMemo/app/src/main/java/com/example/linememo/dao/MemoDatabase.java
+++ LineMemo/app/src/main/java/com/example/linememo/dao/MemoDatabase.java
@@ -25,7 +25,7 @@ public abstract class MemoDatabase extends RoomDatabase {
         synchronized (sLock) {
             if (INSTANCE == null) {
                 INSTANCE = Room
-                        .databaseBuilder(context.getApplicationContext(), MemoDatabase.class, "memo-test-db-4")
+                        .databaseBuilder(context.getApplicationContext(), MemoDatabase.class, "line-memo-db")
                         .build();
             }
             return INSTANCE;
